07059d93d8d2051cac0f34f9aacd28ed19076b8f
Kumar Gaurav <266802710+kumargauravmnnita@users.noreply.github.com>
2026-04-03 15:24:10 +0530

    feat: connect Express server to MongoDB using mongoose

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const app = require("./src/app");
+const connectToDB = require("./src/config/database");
+connectToDB();
 app.listen(3000, () => {
   console.log("Yay!!Server is running on port 3000!");
 });
